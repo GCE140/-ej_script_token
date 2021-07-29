9402e2795149099e018aa53e3a759a5361b243fe
GCE140 <86170226+GCE140@users.noreply.github.com>
2021-07-29 15:09:03 -0300

    Update script_2.0.py

--- script_2.0.py
+++ script_2.0.py
@@ -1,5 +1,6 @@
 import requests, json
 from requests.api import request
+from requests_oauthlib import OAuth2Session
 from requests.exceptions import HTTPError
 from requests.structures import CaseInsensitiveDict
 from datetime import datetime
@@ -8,10 +9,24 @@ ahora = datetime.now()
 formato_fecha = "%d-%m-%Y-%Hh-%Mm-%Ss"
 texto_fecha = ahora.strftime(formato_fecha)
 
-def generar_token(app_id, cli_secret):
+def get_code(client_id, redirect_uri):
+    authorization_base_url = f"https://auth.mercadolibre.com.ar/authorization?response_type=code&client_id={client_id}&redirect_uri={redirect_uri}"
+
+    meli = OAuth2Session(client_id)
+
+    authorization_url, state = meli.authorization_url(authorization_base_url)
+    print("\nDebes conceder el acceso en:\n\n" + authorization_base_url)
+
+    code = input('\nPega la URL de respuesta completa: ')
+    code = code.strip("https://www.mercadolibre.com.ar/?code=")
+
+    print("\nEl código es:\t" + code + "\n")
+    return code
+
+
+def generar_token(app_id, cli_secret, redirect_uri):
     
-    code = input("Ingrese el códigod de autenticación: ")
-    code = code.strip()
+    code = get_code(app_id, redirect_uri)
 
     url = "https://api.mercadolibre.com/oauth/token"
 
@@ -25,8 +40,9 @@ def generar_token(app_id, cli_secret):
 
     json_data = json.loads(resp.text)
 
-    print("Reporte de status: " + str(resp.status_code))
-    print("El token es:\t" + json_data["access_token"])
+    print("Reporte de status: " + str(resp.status_code) + "\n")
+    print("El token es:\t" + json_data["access_token"] + "\n")
+    
     return json_data["access_token"]
 
 
@@ -39,10 +55,10 @@ def crear_log(token, seller_id):
 
     resp = requests.get(url, headers=headers)
 
-    print("Reporte de status: " + str(resp.status_code))
+    print("Reporte de status: " + str(resp.status_code) + "\n")
 
     if resp.status_code != 200:
-        print("Error de acceso al servicio.")
+        print("Error de acceso al servicio.\n")
     else:
         json_data = json.loads(resp.text)
         print()
@@ -67,10 +83,11 @@ def crear_log(token, seller_id):
         nombre_log = "log_seller_" + seller_id + "_" + texto_fecha
 
         log = open(nombre_log+".txt","w")
+
         for j in range (len(ids)):
             log.write("Artículo " + str(j+1) + ":\n" + "ID del ítem:\t\t\t" + str(ids[j])+"\n" + "Título del ítem:\t\t" +str(titles[j])+ "\n" + "ID de la catgoría del ítem:\t" + str(category_ids[j]+ "\n" + "Nombre de la categoría:\t\t" +str(domain_ids[j])+"\n\n"))
         log.close()
-        print("Se ha creado el archivo " + nombre_log + ".txt")
+        print("Se ha creado el archivo " + nombre_log + ".txt\n")
         print()
 
 if __name__ == "__main__":
@@ -80,7 +97,9 @@ if __name__ == "__main__":
     app_id = app_id.strip()
     cli_secret = input("Ingrese el client sectret de la app: ")
     cli_secret = cli_secret.strip()
-
+    redirect_uri = input("Ingrese la URL de redirección URI de la app: ")
+    redirect_uri = redirect_uri.strip()
+    
     sellers = []
 
     cant_revisar = int(input("Ingrese la cantidad de sellers a revisar: "))
@@ -90,8 +109,8 @@ if __name__ == "__main__":
         seller_id = input("Ingrese el Nro ID del seller (ej: 179571326 / 114457637): ")
         sellers.append(seller_id)
     
-    print("\nSellers IDs a revisar: " + str(sellers) + "\n")
+    print("\nSellers IDs a revisar: " + str(sellers))
     
     for j in range (cant_revisar):
-        token = generar_token(app_id, cli_secret)
+        token = generar_token(app_id, cli_secret, redirect_uri)
         crear_log(token, sellers[j])
